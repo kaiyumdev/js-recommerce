835e141fedd8fb29c6f61b152c5ecc8f4beda149
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-01-25 03:11:37 +0600

    start js-ecommerce-site

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+let itemsContainerElement = document.querySelector(".items-container");
+let innerHTML = "";
+items.forEach((item) => {
+  innerHTML += `<div class="item-container">
+  <img class="item-image" src=${item.image} alt="image" />
+  <div class="rating">${item.rating.stars} ⭐ | ${item.rating.count}k</div>
+  <div class="company-name">${item.company}</div>
+  <div class="item-name">${item.item_name}</div>
+  <div class="price">
+    <span class="current-price">${item.current_price}</span>
+    <span class="original-price">${item.original_price}</span>
+    <span class="discount">(${item.discount_percentage}% off)</span>
+  </div>
+  <button class="btn-add-bag">Add To Bag</button>
+  </div>`;
+});
+itemsContainerElement.innerHTML = innerHTML;
