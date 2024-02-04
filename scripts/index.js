ac4bfe4df2918156709ad477f3f833929c3a7fbe
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-02-04 16:26:16 +0600

    function displayItemsOnHomePage call & initialized successfully

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,29 @@
-let itemsContainerElement = document.querySelector(".items-container");
-let innerHTML = "";
-items.forEach((item) => {
-  innerHTML += `<div class="item-container">
-  <img class="item-image" src=${item.image} alt="image" />
-  <div class="rating">${item.rating.stars} ⭐ | ${item.rating.count}k</div>
-  <div class="company-name">${item.company}</div>
-  <div class="item-name">${item.item_name}</div>
-  <div class="price">
-    <span class="current-price">${item.current_price}</span>
-    <span class="original-price">${item.original_price}</span>
-    <span class="discount">(${item.discount_percentage}% off)</span>
-  </div>
-  <button class="btn-add-bag">Add To Bag</button>
-  </div>`;
-});
-itemsContainerElement.innerHTML = innerHTML;
-console.log("I am inside index.js");
+displayItemsOnHomePage();
+let bagItems = [];
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+}
+
+function displayItemsOnHomePage() {
+  let itemsContainerElement = document.querySelector(".items-container");
+
+  let innerHTML = "";
+  items.forEach((item) => {
+    innerHTML += `<div class="item-container">
+    <img class="item-image" src=${item.image} alt="image" />
+    <div class="rating">${item.rating.stars} ⭐ | ${item.rating.count}k</div>
+    <div class="company-name">${item.company}</div>
+    <div class="item-name">${item.item_name}</div>
+    <div class="price">
+      <span class="current-price">${item.current_price}</span>
+      <span class="original-price">${item.original_price}</span>
+      <span class="discount">(${item.discount_percentage}% off)</span>
+    </div>
+    <button class="btn-add-bag" onclick="addToBag(${item.id})">Add To Bag</button>
+    </div>`;
+  });
+  itemsContainerElement.innerHTML = innerHTML;
+}
+
+// console.log("I am inside index.js");
